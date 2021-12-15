c81b2e7fa73906e8bb93ac7f4bfded8bf61d434d
Far Tayeb <chefredr@gmail.com>
2021-12-14 23:17:55 -0600

    Restarting everything

--- app.js
+++
@@ -1,7 +0,0 @@
-function crash() {
-    while(true) {
-        location.reload();
-    }
-}
-
-alert("Warning - Make sure to have all your browser/computer files saved before you press the rocket button.");
