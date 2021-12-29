a76d0de4d7f822cb8e121134387a77871a03b717
Far Tayeb <chefredr@gmail.com>
2021-12-29 03:01:05 -0600

    website update

---
+++ script.js
@@ -0,0 +1,9 @@
+let hat = document.querySelector(".logo");
+let angle = 0;
+let style;
+
+hat.onclick = ()=>{
+    angle += 90;
+    transformation = `transform: rotate(${angle}deg)`
+    hat.setAttribute("style", transformation);
+}
